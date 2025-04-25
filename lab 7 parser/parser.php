565f7d5a15b410dc8fcaf743d9cadb7c436f0be5
Itssuraj-1 <thapasuraj3215@gmail.com>
2025-04-25 19:51:20 +0545

    Update parser.php for lab 7

--- lab 7 parser/parser.php
+++ lab 7 parser/parser.php
@@ -4,21 +4,12 @@ $xml = simplexml_load_file("Bookstore.xml") or die("Error: Cannot load XML file"
 
 //Loop through each <book> element
 foreach ($xml->book as $book) {
-    // Get the title and author
-    $title = (string)$book->title;
-    $author = (string)$book->author;
-
-    // Get the price and currency
-    $price = (float)$book->price;
-    $currency = (string)$book->price['currency'];
-
-    // Get the publication date
-    $pubDate = (string)$book->pub_date;
-
-    // Print the book details
-    echo "Title: $title\n";
-    echo "Author: $author\n";
-    echo "Price: $price $currency\n";
-    echo "Publication Date: $pubDate\n";
-    echo "------------------------\n";
+    echo "title: " . $book->title . "<br>";
+    echo "author: " . $book->author . "<br>";
+    echo "year: " . $book->year . "<br>";
+    echo "price: " . $book->price . "<br>";
+    echo "". $book->description . "<br>";
+    echo "<br>";
+    echo "
+    ". $book->url . EOL;
 }

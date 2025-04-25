fe52ab4eecb9c421cc70518a585c35217c864884
Itssuraj-1 <thapasuraj3215@gmail.com>
2025-04-25 20:01:31 +0545

    php

--- lab 7 parser/parser.php
+++ lab 7 parser/parser.php
@@ -2,14 +2,13 @@
 // Load the XML file
 $xml = simplexml_load_file("Bookstore.xml") or die("Error: Cannot load XML file");
 
-//Loop through each <book> element
+// Loop through each <book> element
 foreach ($xml->book as $book) {
-    echo "title: " . $book->title . "<br>";
-    echo "author: " . $book->author . "<br>";
-    echo "year: " . $book->year . "<br>";
-    echo "price: " . $book->price . "<br>";
-    echo "". $book->description . "<br>";
-    echo "<br>";
-    echo "
-    <hr>";
+    echo "Title: " . $book->title . "<br>";
+    echo "Author: " . $book->author . "<br>";
+    echo "Year: " . $book->year . "<br>";
+    echo "Price: " . $book->price . "<br>";
+    echo "Description: " . $book->description . "<br><br>";
+    echo "<hr>";
 }
+?>
